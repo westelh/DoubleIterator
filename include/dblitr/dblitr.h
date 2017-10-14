df9e8df7987ec123cab626d6fdac5e0c50a8e68d
westelh <westelh@users.noreply.github.com>
2017-10-15 01:45:02 +0900

    Add comment

--- include/dblitr/dblitr.h
+++ include/dblitr/dblitr.h
@@ -2,9 +2,13 @@
 
 namespace elh {
 	class doubled_iterator final {
-		int abs_m;
+		int abs_m;  // absolute position through the range
+
 	public:
+        // construct with absolute position
         doubled_iterator(int abs);
+
+        // get the absolute positon
 		int abs() const noexcept;
 	};
 }
