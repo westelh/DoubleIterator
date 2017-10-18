eedee1159ff21cd1dabc228358a14d4b66c80c6d
Unknown <reo.retriever@gmail.com>
2017-10-18 13:15:53 +0900

    Test for crd()
    Check crd() returns the number given in constructor

--- test/test.cpp
+++ test/test.cpp
@@ -10,6 +10,16 @@ TEST_CASE("abs() returns the number given in constructor", "[]") {
     REQUIRE(h.abs() == 4212);
 }
 
+TEST_CASE("crd() returns the number given in constructor", "[]") {
+    elh::doubled_iterator<10, 10> i{8};
+    REQUIRE(i.crd().first == 8);
+    REQUIRE(i.crd().second == 0);
+
+    elh::doubled_iterator<10, 10> h{23};
+    REQUIRE(h.crd().first == 3);
+    REQUIRE(h.crd().second == 2);
+}
+
 TEST_CASE("abs set and get", "[]") {
     elh::doubled_iterator<10, 10> i{0};
     i.set_abs(100);
